eb9a5b0b44f82ce61dc5eef6cb72cc64c5c77b6d
aaron <aaron.lhh0401@gmail.com>
2024-04-26 00:36:13 -0700

    feat: add database to homepage

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
 
 
-const PostCard = ({  }) => {
+const PostCard = ({ postData }) => {
+    const [timeString, setTimeString] = useState('');
+    useEffect(() => {
+        const prevDate = new Date(postData.created_at);
+        const currentDate = new Date();
+        let diff = (currentDate.getTime() - prevDate.getTime()) / 1000;
+        if (diff < 3600) {
+            setTimeString(`${Math.floor(diff/60)} minutes ago`);
+        } else if (diff < 86400) {
+            setTimeString(`${Math.floor(diff/3600)} hours ago`);
+        } else if (diff < 2620800) {
+            setTimeString(`${Math.floor(diff/86400)} days ago`);
+        } else if (diff < 31449600) {
+            setTimeString(`${Math.floor(diff/2620800)} months ago`);
+        } else {
+            setTimeString(`${Math.floor(diff/31449600)} years ago`);
+        }
+    }, []);
+
     return (
         <div className="post-card">
             <div className="time">
-                Posted at blabla time
+                Posted {timeString}
             </div>
             <div className="gap"></div>
             <Link 
-                to='/detail/1'
+                to={`/detail/${postData.id}`}
                 className="title-name" 
             >
-                title
+                {postData.title}
             </Link>
             <div className="gap"></div>
             <div className="upvotes">
-                blabla upvotes
+                {postData.upvote} upvotes
             </div>
         </div>
     )
--- routes/HomePage.jsx
+++ routes/HomePage.jsx
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
+import { supabase } from "../src/client";
 
 const HomePage = () => {
-    const [listOrder, setListOrder] = useState(1);
+    const [listOrder, setListOrder] = useState(0);
+    const [postList, setPostList] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const dataByTime = await supabase
+                .from('HobbyPost')
+                .select()
+                .order('created_at', {ascending: false});
+
+            const dataByUpvote = await supabase
+                .from('HobbyPost')
+                .select()
+                .order('upvote', {ascending: false});
+            // console.log(dataByUpvote);
+            setPostList([dataByTime.data, dataByUpvote.data]);
+            // console.log(new Date(data['0'].created_at))
+        };
+        fetchData().catch(console.error);
+    }, [])
 
     return (
         <div className="homepage">
@@ -13,6 +33,23 @@ const HomePage = () => {
                         type="radio"
                         id="newest"
                         name='order'
+                        value={0}
+                        className="listOrder-button"
+                        onChange={(e) => {
+                            setListOrder(e.target.value)
+                        }}
+                        checked={listOrder == 0}
+                    />
+                    
+                    <label htmlFor="newest">
+                        Newest
+                    </label>
+                </div>
+                <div>
+                    <input 
+                        type="radio"
+                        id="popular"
+                        name='order'
                         value={1}
                         className="listOrder-button"
                         onChange={(e) => {
@@ -20,37 +57,21 @@ const HomePage = () => {
                         }}
                         checked={listOrder == 1}
                     />
-                    
-                    <label htmlFor="newest">
-                        Newest
-                    </label>
-                    
-                </div>
-                <div>
-                    <input 
-                        type="radio"
-                        id="popular"
-                        name='order'
-                        value={2}
-                        className="listOrder-button"
-                        onChange={(e) => {
-                            setListOrder(e.target.value)
-                        }}
-                        checked={listOrder == 2}
-                    />
                     <label htmlFor="popular">
                         Most Popular
                     </label>
                 </div>
             </div>
             <div className="feeds">
-                <PostCard  />
-                <PostCard  />
-                <PostCard  />
-                <PostCard  />
-                <PostCard  />
-                <PostCard  />
-                <PostCard  />
+                {
+                    postList && 
+                    Object.values(postList[listOrder]).map((value, index) => (
+                        <PostCard  
+                            postData = {value}
+                            key = {index}
+                        />
+                    ))
+                }
             </div>
         </div>
     )
